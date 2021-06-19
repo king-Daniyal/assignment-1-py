5226cc1be1bf1a5a82dfb6bb50bb3b0de6c85baa
king-Daniyal <67100321+king-Daniyal@users.noreply.github.com>
2021-06-19 13:27:01 +0400

    assignment 1 - py
    hah

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # assignment-1-py
+
+### Hello my students 
+## PY - Zillahyps support
+
+## Instructions
+
+at "do.py" you have to understand the answers and interpret them
+
+## Assignment
+
+Combine the methods of this assignment and make your own large function of printing which has 10 lines 
---
+++ do.py
@@ -0,0 +1,17 @@
+#(print) this is the feature which prints the word given ahead of it
+
+print("hi, i am a text") # this is the function the word inside of the "" will come as output 
+
+# lets try numbers
+
+print(2020, "was bad") # this works with numbers and text but the "" shall not be included with the number because it is of no use
+
+print("text 3 \n") 
+
+# the special thing here is that the  "\n" is actually creating a new sentence 
+
+### For the next one we need a clean work space first we create a few "terms" like
+
+a = 5 - 3
+
+print("my answer is \n" "a = ", a)
